a4d6cff2e496437c5c87ff0ef9f00124cba3a57b
unknown <Flamur Deliu>
2019-11-06 14:18:08 +0100

    Updates

---
+++ precache-manifest.06ea0caf8ac139ee1a3064b36b90dea7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bb26f8309d0c8d5df3713e4048b2ee",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "05fdc9aec614a1c83867",
+    "url": "/currency-converter/static/css/2.b2a76980.chunk.css"
+  },
+  {
+    "revision": "f10fffdfa165e216c43c",
+    "url": "/currency-converter/static/css/main.d4116bb8.chunk.css"
+  },
+  {
+    "revision": "05fdc9aec614a1c83867",
+    "url": "/currency-converter/static/js/2.57fb2550.chunk.js"
+  },
+  {
+    "revision": "f10fffdfa165e216c43c",
+    "url": "/currency-converter/static/js/main.c7e15a2d.chunk.js"
+  },
+  {
+    "revision": "c0d28daf08c87ac5e666",
+    "url": "/currency-converter/static/js/runtime-main.f8c82292.js"
+  },
+  {
+    "revision": "5de967ea5968115f48d0263cd926f3ae",
+    "url": "/currency-converter/static/media/background-image.5de967ea.jpg"
+  },
+  {
+    "revision": "bf4c97ed096fec8cdbdbdeeea9a82e72",
+    "url": "/currency-converter/static/media/loading.bf4c97ed.gif"
+  },
+  {
+    "revision": "b5d809e61cfbb8250546fb1fef5a3edd",
+    "url": "/currency-converter/static/media/logo.b5d809e6.JPG"
+  }
+]);
